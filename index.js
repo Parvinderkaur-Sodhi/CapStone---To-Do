7978e267156941fe99c9f694075c466cefb67b52
Parvinderkaur-Sodhi <parvinder@sodhi.in>
2023-09-14 22:09:18 +0530

    First Comit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const tasks = {
+    day: [],
+    work: [],
+};
+
+app.get('/', (req, res) => {
+    res.render('index', { tasks });
+});
+
+app.post('/addTask', (req, res) => {
+    const { task, list } = req.body;
+    tasks[list].push(task);
+    res.redirect('/');
+});
+
+app.post('/completeTask', (req, res) => {
+    const { task, list } = req.body;
+    const index = tasks[list].indexOf(task);
+    if (index !== -1) {
+        tasks[list][index] = tasks[list][index].startsWith('<s>') ? task.slice(3, -4) : `<s>${task}</s>`;
+    }
+    res.redirect('/');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
